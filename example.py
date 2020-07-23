dad46eb227228c7da12a653c3f7eaccdb9146c3f
filippostz <filippositzia@gmail.com>
2020-07-23 16:34:24 +0100

    Update example.py

--- example.py
+++ example.py
@@ -5,15 +5,14 @@ atd = atdlib.atdsession()
 # open new session to the ATD
 atd.open('192.168.224.100', 'user', 'password')
 
-# session was created successfully
-
 # upload a file for analysis and get jobId
 atd.fileup('sample.exe')
-# jobId is 5294
+
+# jobId is xxxx
 
 # Get a list of job tasks
-#atd.jobtasks(5294)
+#atd.jobtasks(xxxx)
 # job initiated one task with id 14326
 
 # getting task status
-#atd.taskstatus(14326)
+#atd.taskstatus(yyy)
